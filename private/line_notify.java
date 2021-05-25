1a1b762c3634ef3d49d2644a59c461dfc7d04968
LEE JIHOON <75344302+lzhxxn@users.noreply.github.com>
2021-05-25 13:25:38 +0900

    src

---
+++ private/line_notify.java
@@ -0,0 +1,23 @@
+//JavaScript - jQuery 
+
+var form = new FormData();
+form.append("message", "SecuLayer");
+
+var settings = {
+  "url": "https://notify-api.line.me/api/notify",
+  "method": "POST",
+  "timeout": 0,
+  "headers": {
+    "Authorization": "Bearer wjEwZ33oSXoVupQhetxvmIEyLFxgqM4dIssTL2raqHi"
+  },
+  "processData": false,
+  "mimeType": "multipart/form-data",
+  "contentType": false,
+  "data": form
+};
+
+$.ajax(settings).done(function (response) {
+  console.log(response);
+});
+
+// 미완
