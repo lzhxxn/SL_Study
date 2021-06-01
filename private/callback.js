34712d7ba511c7819124f7e6479c7d0f22cb3ac3
LEE JIHOON <75344302+lzhxxn@users.noreply.github.com>
2021-06-01 13:19:29 +0900

    src

--- private/callback.js
+++ private/callback.js
@@ -1,1 +1,15 @@
 
+function checkGang(count, link, good) {
+  count < 3 ? link() : good();
+}
+
+function linkGang() {
+  console.log('1일 3깡은 기본입니다. 아래 링크를 통해 깡을 시청해주세요');
+  console.log('https://youtu.be/xqFvYsy4wE4');
+}
+
+function goodGang() {
+  console.log('오늘 할당량은 모두 채우셨습니다! :)')
+}
+
+checkGang(2, linkGang, goodGang);
