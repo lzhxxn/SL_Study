2cae5cddcd582d78cf217d64a586d151e4f019cf
LEE JIHOON <75344302+lzhxxn@users.noreply.github.com>
2021-06-01 13:41:08 +0900

    src

--- private/callback.js
+++ private/callback.js
@@ -13,3 +13,36 @@ function goodGang() {
 }
 
 checkGang(2, linkGang, goodGang);
+
+
+//결과 : 1일 3깡은 기본입니다. 아래 링크를 통해 깡을 시청해주세요. (링크)
+
+
+// 1)
+function findUser(id) {
+  const user = {
+    id: id,
+    name: "User" + id,
+    email: id + "@test.com",
+  };
+  return user;
+}
+
+const user = findUser(1);
+console.log("user:", user);
+
+// 2)
+function findUserAndCallBack(id, cb) {
+  const user = {
+    id: id,
+    name: "User" + id,
+    email: id + "@test.com",
+  };
+  cb(user);
+}
+
+findUserAndCallBack(1, function (user) {
+  console.log("user:", user);
+});
+
+//결과 : user: {id: 1, name: "User1", email: "1@test.com"}
