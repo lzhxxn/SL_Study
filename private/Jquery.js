a167ed21433178cbda41bc23f6dc7757d2cf885c
LEE JIHOON <75344302+lzhxxn@users.noreply.github.com>
2021-06-09 17:12:38 +0900

    src

--- private/Jquery.js
+++ private/Jquery.js
@@ -140,3 +140,45 @@
 </html>
 ```
   
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Insert title here</title>
+    <script
+            src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+  </head>
+  <body>
+    <ul id="list">
+      <li>coffee
+        <ul>
+          <li>black</li>
+          <li>milk</li>
+        </ul>
+      </li>
+      <li>red tea</li>
+      <li id="greentea">green tea</li>
+    </ul>
+
+    <button type="button" name="button" id="deleteGreenBtn">green tea 숨기기</button>
+    <button type="button" name="button" id="delChildBtn">black 숨기기</button>
+
+    <script type="text/javascript">
+      $(document).ready(function () {
+
+        // 리스트 요소를 숨기는 첫번째 방법
+        $('#deleteGreenBtn').click(function () {
+          $('li#greentea').hide();
+        });
+
+        // 리스트 자식 요소를 숨기는 두번째 방법
+        $('#delChildBtn').click(function () {
+          // ul 속 첫번째 li 안에 ul 안에 첫번째 li를 숨긴다.
+          $('#myList li:first ul li:first').hide();
+        });
+      });
+    </script>
+
+  </body>
+</html>
